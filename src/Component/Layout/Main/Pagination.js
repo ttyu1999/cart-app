d8c5b3a6d4f42bbe0215de745856de3ffc361a80
ttyu1999 <love0033621@gmail.com>
2023-08-24 00:12:19 +0800

    修正分頁scroll

--- src/Component/Layout/Main/Pagination.js
+++ src/Component/Layout/Main/Pagination.js
@@ -1,24 +1,20 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { useContext } from 'react';
 import styles from './Pagination.module.scss';
 import { PageContext } from '../../../store/product-context';
+import useScrollTop from "../../../hook/useScrollTop";
 
 const Pagination = () => {
-    const pageCtx = useContext(PageContext);
+    const { scrollToTop } = useScrollTop();
 
+    const pageCtx = useContext(PageContext);
     const { getProductLength, onePageItem, currentPage, setCurrentPage } = pageCtx;
 
     const totalPages = Math.ceil(getProductLength / onePageItem);
 
     const handlePageChange = (page) => {
         setCurrentPage(page);
-
-        const element = document.getElementById('product__content');
-
-        element.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
+        scrollToTop();
     }
 
     let hasPage;
